feac3245e6fef07b25f0d5f037683bdd1e9766c8
AntonBoltnev1101 <smoker008@mail.ru>
2021-12-24 19:21:27 +0300

    Version 0.9.8

--- src/ru/tgbot/chatbot/Main.java
+++ src/ru/tgbot/chatbot/Main.java
@@ -4,23 +4,51 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
 
 
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi metaBot = new TelegramBotsApi(DefaultBotSession.class);
+        BufferedReader dd = new BufferedReader(new InputStreamReader(System.in));
 
-        {
-            try {
-
-                System.out.println("Бот запущен !\n" + "Ожидайте ответа пользователя...");
-
+        try {
+            System.out.println("Бот запущен !\n" + "Ожидайте ответа пользователя...");
+            System.out.println("1: Обычный режим\n" + "2: DDos Attack\n");
+            System.out.print("Выберите режим: ");
+            String ddAt = dd.readLine();
+            if (ddAt.equals("1")) {
+                System.out.println("Выбран обычный режим");
                 metaBot.registerBot(new SelectionOfBots());
-            } catch (TelegramApiException e) {
+            }
+            else if (ddAt.equals("2")) {
+                System.out.println("Выбран режим DDos Attack");
+                metaBot.registerBot(new DDosBot());
+
+                do
+                {
+                    System.out.println("Попытка остановить DDos");
+                    System.out.print("Введите число остановки: ");
+
+                }
+                while (!dd.readLine().equals("1"));
+                System.out.println("Отключение DDos");
+                System.exit(0);
+
+
+
+            }
+
+
+            } catch (TelegramApiException | IOException e) {
                 e.printStackTrace();
             }
-        }
+
+
 
     }
 
--- src/ru/tgbot/chatbot/SelectionOfBots.java
+++ src/ru/tgbot/chatbot/SelectionOfBots.java
@@ -1,5 +1,7 @@
 package ru.tgbot.chatbot;
 
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -7,8 +9,11 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -35,103 +40,134 @@ public class SelectionOfBots extends TelegramLongPollingBot {
 
         Collections.shuffle(arrayList);
 
-        if (update.hasMessage() )
+        if (update.hasMessage())
         {
 
             Message message = update.getMessage();
 
-            System.out.println(update.getMessage()
-                    .getFrom()
-                    .getFirstName() + ", написал: "
-                    + message.getText());
+            if (update.getMessage().hasText()) {
 
-
-            if (message.getText().equals("/start"))
-            {
-                SendMessage startMessage = new SendMessage();
-                startMessage.setChatId(message.getChatId().toString());
-                startMessage.setText("Вас приветствует @METAELAY !\n" +
-                        update.getMessage().getFrom().getFirstName()  + ", введите свое сообщение и ожидайте ответа...");
-
-                try {
-                    execute(startMessage);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            else if (message.getText().equals("/exit"))
-            {
-                SendMessage endMess = new SendMessage();
-                endMess.setChatId(message.getChatId().toString());
-                endMess.setText("До встречи, " + update.getMessage().getFrom().getFirstName() + " !");
-
-
-                try
-                {
-                    execute(endMess);
-                    System.out.println("Бот выключен");
-                    System.exit(0);
-                }
-
-                catch (TelegramApiException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-
-            else if (message.getText().equals("/pic"))
-            {
-
-
-                SendPhoto sendPhoto = new SendPhoto();
-                sendPhoto.setChatId(message.getChatId().toString());
-                sendPhoto.setPhoto(arrayList.get(0));
-                sendPhoto.setCaption("Лягушка");
-
-                try
-                {
-                    execute(sendPhoto);
-                }
-                catch (TelegramApiException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-
-                    else
-                    {
-                        SendMessage repeaterBot = new SendMessage();
-                        SendMessage wait = new SendMessage();
-
-                        wait.setChatId(message.getChatId().toString());
-                        wait.setText("Вы написали: " + message.getText() + "\n" + "Ожидайте ответа Бота");
-
-                        repeaterBot.setChatId(message.getChatId().toString());
+                System.out.println(update.getMessage()
+                        .getFrom()
+                        .getFirstName() + ", написал: "
+                        + message.getText());
 
 
 
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            if (message.getText().equals("/start")) {
+                    SendMessage startMessage = new SendMessage();
+                    startMessage.setChatId(message.getChatId().toString());
+                    startMessage.setText("Вас приветствует @METAELAY !\n" +
+                            update.getMessage().getFrom().getFirstName() + ", введите свое сообщение и ожидайте ответа...");
 
-
-                        try
-                        {
-                            execute(wait);
-                            System.out.print("Ответ пользователю " + update.getMessage()
-                                    .getFrom()
-                                    .getFirstName() + ": ");
-                            String txt = reader.readLine();
-                            repeaterBot.setText("Бот говорит: " + txt);
-                            execute(repeaterBot);
-                        }
-
-                        catch (IOException | TelegramApiException e)
-                        {
-                            e.printStackTrace();
-                        }
+                    try {
+                        execute(startMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
                     }
+                } else if (message.getText().equals("/exit")) {
+                    SendMessage endMess = new SendMessage();
+                    endMess.setChatId(message.getChatId().toString());
+                    endMess.setText("До встречи, " + update.getMessage().getFrom().getFirstName() + " !");
+
+
+                    try {
+                        execute(endMess);
+                        System.out.println("Бот выключен");
+                        System.exit(0);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                } else if (message.getText().equals("/pic")) {
+
+
+                    SendPhoto sendPhoto = new SendPhoto();
+                    sendPhoto.setChatId(message.getChatId().toString());
+                    sendPhoto.setPhoto(arrayList.get(0));
+                    sendPhoto.setCaption("Лягушка");
+
+                    try {
+                        execute(sendPhoto);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    SendMessage repeaterBot = new SendMessage();
+                    SendMessage wait = new SendMessage();
+
+                    wait.setChatId(message.getChatId().toString());
+                    wait.setText("Вы написали: " + message.getText() + "\n" + "Ожидайте ответа Бота");
+
+                    repeaterBot.setChatId(message.getChatId().toString());
+
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+                    try {
+                        execute(wait);
+                        System.out.print("Ответ пользователю " + update.getMessage()
+                                .getFrom()
+                                .getFirstName() + ": ");
+                        String txt = reader.readLine();
+                        repeaterBot.setText("Бот говорит: " + txt);
+                        execute(repeaterBot);
+                    } catch (IOException | TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+                     else if (update.getMessage().hasVideo()) {
+            Video video = update.getMessage().getVideo();
+            String fileId = video.getFileId();
+            String fileName = video.getFileName();
+
+            System.out.println(update.getMessage().getFrom().getFirstName()
+                    + " прислал вам видео: " + fileName);
+
+
+//                System.out.println(fileName);
+//                System.out.println(fileId);
+
+            try {
+                downloadFile(fileName,fileId);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
             System.out.println("Ожидайте ответа от " + update.getMessage().getFrom().getFirstName() + "...");
         }
+
+
+
+    }
+
+    public void downloadFile(String fileName, String fileId) throws IOException {
+        URL url = new URL("https://api.telegram.org/bot" + getBotToken() + "/getFile?file_id=" + fileId);
+//        System.out.println("Запрос на скачивание файла через браузер " + url);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        String getFileRsponse = br.readLine();
+
+        JSONObject jresult = new JSONObject(getFileRsponse);
+        JSONObject path = jresult.getJSONObject("result");
+        String file_path = path.getString("file_path");
+//        System.out.println("Путь файла на сервере Telegram: " + file_path);
+
+//        File localFile = new File("D:/IDEAProjects/TelegramBot/resources/uploadfiles/" + fileName);
+        File localFile = new File("D:/IDEAProjects/TelegramBot/resources/uploadfiles/" + fileName);
+
+        InputStream is = new URL("https://api.telegram.org/file/bot" + getBotToken() + "/" + file_path).openStream();
+
+        FileUtils.copyInputStreamToFile(is,localFile);
+
+        br.close();
+        is.close();
+
+        System.out.println("Файл загружен");
+        System.out.println("Путь файла: " + localFile.getAbsolutePath());
 
     }
 }
